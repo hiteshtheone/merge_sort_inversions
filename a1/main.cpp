d666a527dbaab91177bfde455c1a5dd6afa11cfa
hiteshtheone <hiteshtheone@gmail.com>
2015-03-16 23:42:12 +0530

    Initial Commit

---
+++ a1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a1
+//
+//  Created by hambarkh on 16/03/15.
+//  Copyright (c) 2015 Akola. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
